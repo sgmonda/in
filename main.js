7d93600b30684922bcb53b231f296ad40c712fba
sgmonda <sgmonda@gmail.com>
2012-11-21 22:06:22 +0100

    Basic behavoir and simple README created

--- main.js
+++ main.js
@@ -1,42 +1,82 @@
-var stdin = process.openStdin();
-
-// Parse command line arguments
-module.exports.opts = (function(argv){
+/**
+ * Parses command line options
+ * @param {function} arguments vector
+ */
+module.exports.getopt = function(options){
     
-    // Options map
-    var opts = {};
-    
+    var argv = process.argv,     // Arguments
+        opts = {},               // Options map
+        arg,                     // Every argument
+        spected;
+        
     // Arguments parsing
-    
-    //argv[0] == 'node' && argv.shift();
     argv.shift() && argv.shift();
-    
-    for(var i = 0, len = argv.length; i < len; i++){
-        if(argv[i].charAt(0) == '-'){
-            argv[i] = argv[i].replace(/-/g, '');
-            opts[argv[i]] = true;
+
+    for(var i = 0, len = argv.length; i < len, arg = argv[i]; i++){
+        var opt = null, optname = null;
+        if(arg.charAt(0) == '-') {
+            opt = {}, spected = null;
+            if(arg.charAt(1) == '-') {
+                // It's a long option                
+                optname = arg.substring(2);
+                spected = options[optname];
+                if(!spected) throw new Error('Unknown option: --' + optname);                
+            } else {
+                // It's a short option
+                for(var name in options){
+                    if(options[name].key == arg.substring(1)){
+                        optname = name;
+                        break;
+                    }
+                }
+                spected = options[optname];
+                if(!spected) throw new Error('Unknown option: -' + arg.substring(1));                
+            }
+
+            // Arguments asociated with this option
+            if(spected.args == 1){
+                opt.args = argv[++i];
+            } else if(spected.args) {
+                opt.args = [];
+                for(var j = i + 1; j < i + 1 + (spected.args || 0); j++){
+                    opt.args.push(argv[j]);
+                }
+                i += spected.args;
+            }else{
+                opt = true;
+            }
+            
+            if(opt){
+                opts[optname] = opt;
+            }
+        } else {
+            if(!opts.args) opts.args = [];
+            opts.args.push(argv[i]);
         }
+        
+        
     }
 
-    
-    console.log(argv);    
-    console.log(opts);
     return opts;
     
-})(process.argv);
+};
 
+/**
+ * Reads the complete standard input
+ * @param {function} callback
+ */
+module.exports.read = function(callback){
 
-// Reads from standard input
-module.exports.readInput = function(callback){
-    
     if(!callback) throw new Error('no callback provided to readInput() call');
-    
+
     var inputdata = '';
-    stdin.on('data', function(text){
+    
+    process.stdin.resume();    
+    process.stdin.on('data', function(text){
         inputdata += String(text);
     });
-    stdin.on('end', function(){
-        callback && callback(null, inputdata);
+    process.stdin.on('end', function(){
+        callback && callback(inputdata);
     });
-
+    
 };
